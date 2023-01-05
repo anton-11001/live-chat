2601402fbb2cd31468e9fbef8d1515417a6cc7ee
Anton <krava34007@gmail.com>
2023-01-05 09:03:44 +0000

    remove errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import "./app.css";
-import LongPulling from "./LongPulling";
-import EventSourcing from "./EventSourcing";
-import WebSock from "./WebSock";
 
 function App() {
   return <div>Hi chat</div>;
