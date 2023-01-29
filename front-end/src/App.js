77f6add789858a51b81667957aa6e5a96caf38b8
Anton <krava34007@gmail.com>
2023-01-29 16:41:55 +0000

    improve styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import "./App.css";
 
 const SERVER_URL = "ws://localhost:5000";
 
@@ -72,15 +73,21 @@ function App() {
 
   if (!connected) {
     return (
-      <form onSubmit={connect}>
-        <input
-          value={username}
-          onChange={handleUsernameChange}
-          type="text"
-          placeholder="Enter your name"
-        />
-        <button type="submit">Join</button>
-      </form>
+      <main className="chat-page">
+        <form className="join-panel" onSubmit={connect}>
+          <h1>Live Chat</h1>
+          <p>Choose a name and join the room.</p>
+          <div className="join-row">
+            <input
+              value={username}
+              onChange={handleUsernameChange}
+              type="text"
+              placeholder="Enter your name"
+            />
+            <button type="submit">Join</button>
+          </div>
+        </form>
+      </main>
     );
   }
 
@@ -89,29 +96,52 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={sendMessage}>
-        <input
-          value={value}
-          onChange={handleValueChange}
-          type="text"
-          placeholder="Enter your message"
-        />
-        <button type="submit">Send</button>
-      </form>
+    <main className="chat-page">
+      <section className="chat-shell">
+        <header className="chat-header">
+          <div>
+            <h1>Live Chat</h1>
+            <span>Signed in as {username}</span>
+          </div>
+          <div className="status">Online</div>
+        </header>
 
-      {messages.map((m) => (
-        <div key={m.id}>
-          {m.event === EVENTS.CONNECTION ? (
-            <div>User {m.username} has connected</div>
-          ) : (
-            <div>
-              {m.username}: {m.message}
-            </div>
+        <div className="messages">
+          {messages.length === 0 && (
+            <div className="empty-state">No messages yet. Say hello.</div>
           )}
+
+          {messages.map((m) => (
+            <div key={m.id}>
+              {m.event === EVENTS.CONNECTION ? (
+                <div className="connection-message">
+                  User {m.username} has connected
+                </div>
+              ) : (
+                <div
+                  className={
+                    m.username === username ? "message message-own" : "message"
+                  }
+                >
+                  <span>{m.username}</span>
+                  <p>{m.message}</p>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+
+        <form className="message-form" onSubmit={sendMessage}>
+          <input
+            value={value}
+            onChange={handleValueChange}
+            type="text"
+            placeholder="Enter your message"
+          />
+          <button type="submit">Send</button>
+        </form>
+      </section>
+    </main>
   );
 }
 
